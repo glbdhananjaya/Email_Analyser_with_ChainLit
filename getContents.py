cbfcee60054f2c6ef9e7c5bc307d473b47f62381
bhashitha dhananjaya <bhashithadhananjaya@gmail.com>
2023-11-17 14:20:40 +0530

    decomposed img,svg and any other styles

--- getContents.py
+++ getContents.py
@@ -3,8 +3,9 @@ import os
 from bs4 import BeautifulSoup
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from textblob import TextBlob
+import re
 
-def process_json_with_sentiment_analysis():
+def process_json_with_sentiment_analysis(batch_size=100):
     current_directory = os.getcwd()
     file_name = "data.json"
     full_path = os.path.join(current_directory, file_name)
@@ -17,20 +18,32 @@ def process_json_with_sentiment_analysis():
 
     def convert_html_to_text(html):
         soup = BeautifulSoup(html, 'html.parser')
-        return soup.get_text()
+        for script_or_style in soup(['script', 'style']):
+            script_or_style.decompose()
+
+        for img in soup.find_all('img', 'svg'):
+            img.decompose()
+
+        text_with_emojis = soup.get_text()
+        text_without_emojis = re.sub(r'[\U00010000-\U0010ffff]', '', text_with_emojis)
+        
+        return text_without_emojis.strip()
 
     plain_texts = [convert_html_to_text(content) if any(tag for tag in BeautifulSoup(content, 'html.parser').find_all()) else content for content in contents]
 
     analyses = []
 
-    for index, text in enumerate(plain_texts):
-        analysis = {
-            "Subject": subjects[index],
-            "content": text,
-        }
-        analyses.append(analysis)
+    for i in range(0, len(plain_texts), batch_size):
+        batch_texts = plain_texts[i:i + batch_size]
+        batch_subjects = subjects[i:i + batch_size]
 
-    # Return a JSON-compatible string
+        for index, text in enumerate(batch_texts):
+            analysis = {
+                "Subject": batch_subjects[index],
+                "content": text,
+            }
+            analyses.append(analysis)
+
     return json.dumps(analyses)
 
 result = process_json_with_sentiment_analysis()
